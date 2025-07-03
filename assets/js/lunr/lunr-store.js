3f5d6515b1b033ca1dcf967bd5baa6bfd887ade0
Hege-official <Hege-official@users.noreply.github.com>
2025-07-03 04:54:26 +0000

    deploy: 5e25c567c88709347d209bbac5fed3ae4c659edc

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "测试页面",
+        "excerpt":"这只是测试  ","categories": ["维护"],
+        "tags": ["维护"],
+        "url": "/%E7%BB%B4%E6%8A%A4/test1/",
+        "teaser": null
+      }]
